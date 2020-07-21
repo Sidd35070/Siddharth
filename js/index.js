2cb6705a76f444b490556843761f00a49bc2c326
unknown <sidd35070@gmail.com>
2020-07-21 11:46:14 +0530

    typing effect done, starting testing

--- js/index.js
+++ js/index.js
@@ -47,4 +47,26 @@ function navSlide() {
   //Animation
 }
 
+console.log("yes");
+const texts = ["Developer", "Designer", "Blogger", "Public Speaker"];
+var count = 0;
+var index = 0;
+var currentText = "";
+var letters = "";
+function type() {
+  if (index === texts.length) {
+    index = 0;
+  }
+  currentText = texts[index];
+  letters = currentText.slice(0, count++);
+  document.querySelector(".type").textContent = letters;
+
+  if (count - 1 === currentText.length) {
+    count = 0;
+    index++;
+
+    setTimeout(type, 1000);
+  } else setTimeout(type, 250);
+}
+type();
 navSlide();
